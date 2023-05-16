5014b714e0945fd7ef27c405aaf3343240774b74
GuchiEg <jamesdarcyhudson@gmail.com>
2023-05-16 21:51:20 +1000

    app code

---
+++ lib/main.dart
@@ -0,0 +1,288 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_joystick/flutter_joystick.dart';
+import 'dart:async';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+// import 'package:location/location.dart';
+// import 'package:location_permissions/location_permissions.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io' show Platform;
+
+Uuid _ESP32_UUID = Uuid.parse("053e3eda-9fca-48cb-9b59-0089e38c4d50");
+Uuid _ESP32_WRITE = Uuid.parse("a90c323c-eabd-4121-94a5-7e36a6e99a23");
+
+void main() {
+  runApp(const JoystickExampleApp());
+}
+
+const ballSize = 20.0;
+const step = 10.0;
+
+class JoystickExampleApp extends StatelessWidget {
+  const JoystickExampleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Joystick Example'),
+        ),
+        body: const MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final flutterReactiveBle = FlutterReactiveBle();
+  List<DiscoveredDevice> _foundBleUARTDevices = [];
+  // mark as late as scans and connections will be initialised later
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late Stream<ConnectionStateUpdate> _currentConnectionStream;
+  late StreamSubscription<ConnectionStateUpdate> _connection;
+  late QualifiedCharacteristic _rxCharacteristic;
+  bool _scanning = false;
+  bool _connected = false;
+  List<int> _controlBytes = [];
+  String _logTexts = "";
+
+  // Location location = new Location();
+  late PermissionStatus _permissionGranted;
+  void refreshScreen() {
+    setState(() {});
+  }
+
+  void _sendData(double x, double y) async {
+    if (!_connected) {
+      return;
+    }
+    
+    // Get X and Y from the Joystick.
+    // Invert X
+    // Calculate R+L (Call it V): V =(100-ABS(X)) * (Y/100) + Y
+    // Calculate R-L (Call it W): W= (100-ABS(Y)) * (X/100) + X
+    // Calculate R: R = (V+W) /2
+    // Calculate L: L= (V-W)/2
+    // Do any scaling on R and L your hardware may require.
+    // Send those values to your Robot.
+    // Go back to 1.
+    // x = x * 100;
+    // y = y * 100;
+    x = -x;
+    double v = (1 - x.abs()) * (y) + y;
+    double w = (1 - y.abs()) * (x) + x;
+    double rightMotor = -(v + w) / 2;
+    double leftMotor = -(v - w) / 2;
+    print(leftMotor);
+    print(rightMotor);
+    int motorControl = 0;
+
+    // LEFT MOTOR A
+    if (leftMotor < 0) {
+      motorControl = motorControl + 1; 
+      leftMotor = leftMotor.abs();
+    } 
+    if (rightMotor < 0) {
+      motorControl = motorControl + 2;
+      leftMotor = rightMotor.abs();
+    }
+    int motorA = (leftMotor * 255).toInt();
+    int motorB = (rightMotor * 255).toInt();
+    List<int> test = [motorControl, motorA, motorB];
+    await flutterReactiveBle.writeCharacteristicWithResponse(_rxCharacteristic,
+        value: test);
+  }
+
+  void _disconnect() async {
+    await _connection.cancel();
+    _connected = false;
+    refreshScreen();
+  }
+
+  void _stopScan() async {
+    await _scanStream.cancel();
+    _scanning = false;
+    refreshScreen();
+  }
+
+  void _startScan() async {
+    // _permissionGranted = await location.hasPermission();
+    // print(_permissionGranted);
+    // if (_permissionGranted == PermissionStatus.denied) {
+    //   _permissionGranted = await location.requestPermission();
+    //   if (_permissionGranted != PermissionStatus.granted) {
+    //     return;
+    //   }
+    // }
+    var status = await Permission.bluetooth.status;
+
+    if (status.isGranted) {
+      print('Location permission is granted');
+      // scanForDevices();
+    } else {
+      print('Location permission is not granted');
+      await [
+        Permission.location,
+        Permission.bluetooth,
+        Permission.bluetoothAdvertise,
+        Permission.bluetoothConnect,
+        Permission.bluetoothScan,
+      ].request();
+
+      status = await Permission.location.status;
+
+      print(status.isGranted);
+
+      // scanForDevices();
+    }
+    _foundBleUARTDevices = [];
+    _scanning = true;
+    refreshScreen();
+    _scanStream = flutterReactiveBle
+        .scanForDevices(withServices: [_ESP32_UUID]).listen((device) {
+      // connect here
+      onConnectDevice(device);
+      // if (_foundBleUARTDevices.every((element) =>
+      // element.id != device.id)) {
+      //   _foundBleUARTDevices.add(device);
+      //   refreshScreen();
+      // }
+    }, onError: (Object error) {
+      print("ERROR while scanning:$error \n");
+      _logTexts = "${_logTexts}ERROR while scanning:$error \n";
+      refreshScreen();
+    });
+  }
+
+  void onConnectDevice(device) {
+    _currentConnectionStream = flutterReactiveBle.connectToAdvertisingDevice(
+      id: device.id,
+      prescanDuration: Duration(seconds: 1),
+      withServices: [_ESP32_UUID],
+    );
+    _logTexts = "";
+    refreshScreen();
+    _connection = _currentConnectionStream.listen((event) {
+      var id = event.deviceId.toString();
+      // print(event);
+      switch (event.connectionState) {
+        case DeviceConnectionState.connecting:
+          {
+            
+            _logTexts = "${_logTexts}Connecting to $id\n";
+            break;
+          }
+        case DeviceConnectionState.connected:
+          {
+            _connected = true;
+            _logTexts = "${_logTexts}Connected to $id\n";
+            _rxCharacteristic = QualifiedCharacteristic(
+                serviceId: _ESP32_UUID,
+                characteristicId: _ESP32_WRITE,
+                deviceId: event.deviceId);
+            
+            break;
+          }
+        case DeviceConnectionState.disconnecting:
+          {
+            _connected = false;
+            _logTexts = "${_logTexts}Disconnecting from $id\n";
+            break;
+          }
+        case DeviceConnectionState.disconnected:
+          {
+            _logTexts = "${_logTexts}Disconnected from $id\n";
+            break;
+          }
+      }
+      refreshScreen();
+    }, onError: (Object error) {
+      print("ERROR while connecting:$error \n");
+      _logTexts = "${_logTexts}ERROR while connecting:$error \n";
+      refreshScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          const Text("Status messages:"),
+          Container(
+              margin: const EdgeInsets.all(3.0),
+              width: 1400,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.blue, width: 2)),
+              height: 90,
+              child: Scrollbar(
+                  child: SingleChildScrollView(child: Text(_logTexts)))),
+          Joystick(
+            mode: JoystickMode.all,
+            listener: (details) {
+              _sendData(details.x, details.y);
+            },
+          ),
+        ],
+      )
+
+          // child: Stack(
+          //   children: [
+          //     Container(
+          //       color: Colors.green,
+          //     ),
+          //     Align(
+          //       alignment: const Alignment(0, 0.8),
+          //       child:
+          //     ),
+          //   ],
+          // ),
+          ),
+      persistentFooterButtons: [
+        Container(
+          height: 35,
+          child: Column(
+            children: [
+              if (_scanning)
+                const Text("Scanning: Scanning")
+              else
+                const Text("Scanning: Idle"),
+              if (_connected)
+                const Text("Connected")
+              else
+                const Text("disconnected."),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: !_scanning && !_connected ? _startScan : () {},
+          child: Icon(
+            Icons.play_arrow,
+            color: !_scanning && !_connected ? Colors.blue : Colors.grey,
+          ),
+        ),
+        ElevatedButton(
+            onPressed: _scanning ? _stopScan : () {},
+            child: Icon(
+              Icons.stop,
+              color: _scanning ? Colors.blue : Colors.grey,
+            )),
+        ElevatedButton(
+            onPressed: _connected ? _disconnect : () {},
+            child: Icon(
+              Icons.cancel,
+              color: _connected ? Colors.blue : Colors.grey,
+            ))
+      ],
+    );
+  }
+}
---
+++ test/test.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:flutter_joystick/flutter_joystick.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const ballSize = 20.0;
+const step = 10.0;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the consoale where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class JoystickExample extends StatefulWidget {
+  const JoystickExample({Key? key}) : super(key: key);
+
+  @override
+  _JoystickExampleState createState() => _JoystickExampleState();
+}
+
+class _JoystickExampleState extends State<JoystickExample> {
+  double _x = 100;
+  double _y = 100;
+  JoystickMode _joystickMode = JoystickMode.all;
+
+  @override
+  void didChangeDependencies() {
+    _x = MediaQuery.of(context).size.width / 2 - ballSize / 2;
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      // appBar: AppBar(
+      //   title: const Text('Joystick'),
+      //   actions: [
+      //     JoystickModeDropdown(
+      //       mode: _joystickMode,
+      //       onChanged: (JoystickMode value) {
+      //         setState(() {
+      //           _joystickMode = value;
+      //         });
+      //       },
+      //     ),
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.green,
+            ),
+            Ball(_x, _y),
+            Align(
+              alignment: const Alignment(0, 0.8),
+              child: Joystick(
+                mode: _joystickMode,
+                listener: (details) {
+                  setState(() {
+                    _x = _x + step * details.x;
+                    _y = _y + step * details.y;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Ball extends StatelessWidget {
+  final double x;
+  final double y;
+
+  const Ball(this.x, this.y, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: x,
+      top: y,
+      child: Container(
+        width: ballSize,
+        height: ballSize,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.redAccent,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 3),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
